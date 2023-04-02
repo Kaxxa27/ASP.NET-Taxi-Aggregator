d358524ba54f8abd0d43cd860a73f822a95b0239
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-04-02 11:16:11 +0300

    The Controllers folder with HomeController has been added.

---
+++ TaxiApplication/TaxiApplication/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaxiApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+
+    }
+}

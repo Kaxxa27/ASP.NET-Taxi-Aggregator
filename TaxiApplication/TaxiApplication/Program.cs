3d6b87c5ad39051872ef4cab36d23a91ac03bb91
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-04-02 11:19:09 +0300

    Pattern MVC has been added in project.

--- TaxiApplication/TaxiApplication/Program.cs
+++ TaxiApplication/TaxiApplication/Program.cs
@@ -1,15 +1,20 @@
-namespace TaxiApplication
+namespace TaxiApplication;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
+        var builder = WebApplication.CreateBuilder(args);
 
-            app.MapGet("/", () => "Hello World!");
+        builder.Services.AddControllersWithViews();
+        var app = builder.Build();
 
-            app.Run();
-        }
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}"
+            );
+
+        app.Run();
     }
 }

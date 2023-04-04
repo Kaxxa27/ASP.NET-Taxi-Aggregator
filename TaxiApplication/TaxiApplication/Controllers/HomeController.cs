7a946ffec74842f730514d818a27fe0dd7c2ea1e
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-04-04 21:29:40 +0300

    The implemented method of adding a user in the Home controller to check the work with the database.

--- TaxiApplication/TaxiApplication/Controllers/HomeController.cs
+++ TaxiApplication/TaxiApplication/Controllers/HomeController.cs
@@ -1,10 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.DataProtection.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using TaxiApplication.DAL.Interfaces;
+using TaxiApplication.Domain.Entity;
+using TaxiApplication.Domain.Enum;
 
 namespace TaxiApplication.Controllers
 {
     public class HomeController : Controller
     {
+
+        IUnitOfWork _repository;
+        public HomeController(IUnitOfWork repository)
+        {
+            _repository = repository; 
+        }
         public IActionResult Index() => View();
 
+        [HttpGet]
+        public IActionResult AddItem() => View();
+        [HttpPost]
+        public IActionResult AddItem(User user)
+        {
+            _repository.UserRepository.AddAsync(user);
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
--- TaxiApplication/TaxiApplication/Program.cs
+++ TaxiApplication/TaxiApplication/Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using TaxiApplication.DAL;
+using TaxiApplication.DAL.Interfaces;
+using TaxiApplication.DAL.Repositories;
+using TaxiApplication.Domain.Entity;
+
 namespace TaxiApplication;
 
 public class Program
@@ -7,7 +13,16 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         builder.Services.AddControllersWithViews();
+
+        builder.Services.AddScoped<IRepository<User>, EfRepository<User>>();
+        builder.Services.AddScoped<IUnitOfWork,  EfUnitOfWork>();
+
+        var connection = builder.Configuration.GetConnectionString("SQLiteConnection");
+        builder.Services.AddDbContext<TaxiApplicationDbContext>(options =>
+            options.UseSqlite(connection));
+
         var app = builder.Build();
+
 
 
         app.MapControllerRoute(

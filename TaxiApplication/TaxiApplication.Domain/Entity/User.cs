28db6712333c48488e3402499b503ae4ead81de8
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-04-04 22:19:34 +0300

    Added attributes for the User class in Domain.

--- TaxiApplication/TaxiApplication.Domain/Entity/User.cs
+++ TaxiApplication/TaxiApplication.Domain/Entity/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Net.Http.Headers;
 using System.Security.Principal;
@@ -11,10 +12,25 @@ namespace TaxiApplication.Domain.Entity;
 
 public class User : Entity
 {
-    public Role Role { get; set; } 
-    public string Name { get; set; } = string.Empty;
-    public string Surname { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
+	[Required(ErrorMessage = "Введите роль пользователя.")]
+    public Role Role { get; set; }
+
+	[Required(ErrorMessage = "Укажите имя пользователя.")]
+	[StringLength(50, MinimumLength = 2, ErrorMessage = "Длина имени должна быть от 2 до 50 символов.")]
+	public string Name { get; set; } = string.Empty;
+	
+	[Required(ErrorMessage = "Укажите фамилию пользователя.")]
+	[StringLength(50, MinimumLength = 2, ErrorMessage = "Длина фамилии должна быть от 2 до 50 символов.")]
+	public string Surname { get; set; } = string.Empty;
+
+	[Required(ErrorMessage = "Укажите пароль.")]
+	public string Password { get; set; } = string.Empty;
+
+	[EmailAddress(ErrorMessage = "Некорректный адрес электронной почты.")]
+	[Required(ErrorMessage = "Укажите вашу электронную почту.")]
 	public string Email { get; set; } = string.Empty;
-    public string PhoneNumber { get; set; } = string.Empty;
+
+	[Phone(ErrorMessage = "Некорректный номер телефона.")]
+	[Required(ErrorMessage = "Укажите ваш номер телефона.")]
+	public string PhoneNumber { get; set; } = string.Empty;
 }

7b22e900f1bdc74e25b782b7b837d304a5de78ff
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-04-04 20:43:58 +0300

    Added the User class to Domain\Entity.

---
+++ TaxiApplication/TaxiApplication.Domain/Entity/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using TaxiApplication.Domain.Enum;
+
+namespace TaxiApplication.Domain.Entity;
+
+public class User : Entity
+{
+    public Role Role { get; set; } 
+    public string Name { get; set; } = string.Empty;
+    public string Surname { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+	public string Email { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+}

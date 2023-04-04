8dd232e4d09d69ca53788c0b329281cc56862627
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-04-05 00:49:20 +0300

    Added the IUserService interface to BLL\Interfaces.

---
+++ TaxiApplication/TaxiApplication.BLL/Interfaces/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using TaxiApplication.Domain.Entity;
+using TaxiApplication.Domain.Responce.Interfaces;
+
+namespace TaxiApplication.BLL.Interfaces;
+
+public interface IUserService
+{
+	Task<IBaseResponse<User>> GetUser(int id);
+	Task<IBaseResponse<IEnumerable<User>>> GetAllUsers();
+	Task<IBaseResponse<User>> AddUser(User user);
+	Task<IBaseResponse<bool>> DeleteUser(int id);
+	Task<IBaseResponse<User>> FirstOrDefault(Func<User, bool> filter);
+}

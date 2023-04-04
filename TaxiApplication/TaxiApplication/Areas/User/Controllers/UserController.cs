d282c1fb461af4c6fa885f0830e2082c47440b98
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-04-05 00:41:57 +0300

    The User area with the UserService controller has been added.

---
+++ TaxiApplication/TaxiApplication/Areas/User/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaxiApplication.BLL.Interfaces;
+
+namespace TaxiApplication.WEB.Areas.User.Controllers
+{
+	[Area("User")]
+	public class UserController : Controller
+	{
+		IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+		[HttpGet]
+        public IActionResult GetAllUsers()
+		{
+			return View(_userService.GetAllUsers().Result.Data);
+		}
+	}
+}

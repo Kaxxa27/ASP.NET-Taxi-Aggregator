c863419eaf8bd1474190047b6b3fc9ed23ced855
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-04-04 21:26:26 +0300

    Added the EfUnitOfWork class to DAL\Repositories.

---
+++ TaxiApplication/TaxiApplication.DAL/Repositories/EfUnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxiApplication.DAL.Interfaces;
+using TaxiApplication.Domain.Entity;
+
+namespace TaxiApplication.DAL.Repositories;
+
+public class EfUnitOfWork : IUnitOfWork
+{
+	private readonly TaxiApplicationDbContext _context;
+	private readonly IRepository<User> _userRepository;
+
+	public EfUnitOfWork(TaxiApplicationDbContext context)
+	{
+		_context = context;
+		_userRepository = new EfRepository<User>(context);
+	}
+
+	IRepository<User> IUnitOfWork.UserRepository => _userRepository;
+
+	public async Task CreateDatabaseAsync()
+	{
+		await _context.Database.EnsureCreatedAsync();
+	}
+	public async Task RemoveDatbaseAsync()
+	{
+		await _context.Database.EnsureDeletedAsync();
+	}
+	public async Task SaveAllAsync()
+	{
+		await _context.SaveChangesAsync();
+	}
+}

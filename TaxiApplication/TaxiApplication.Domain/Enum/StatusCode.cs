38973b552411b2f4c49aa8a27dc8813162890b3c
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-04-05 00:40:41 +0300

    The StatusCode enum has been updated.

--- TaxiApplication/TaxiApplication.Domain/Enum/StatusCode.cs
+++ TaxiApplication/TaxiApplication.Domain/Enum/StatusCode.cs
@@ -8,5 +8,17 @@ namespace TaxiApplication.Domain.Enum;
 
 public enum StatusCode
 {
+	/// <summary>
+	/// User StatusCode 1**-> 
+	/// 0-1*   -> 0 -> UserNotFound
+	///			  1 -> UserAlreadyExist
+	/// 0-1**  ->
+	///  
+	/// </summary>
+	UserNotFound = 100,
+	UserAlredyExist = 110,
+	
+	AllError = 404,
 	OK = 777,
+
 }

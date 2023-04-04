ec6002439d671643d816ac08cc66222433135acd
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-04-04 21:26:11 +0300

    Added the EfRepository class to DAL\Repositories.

---
+++ TaxiApplication/TaxiApplication.DAL/Repositories/EfRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxiApplication.DAL.Interfaces;
+using TaxiApplication.Domain.Entity;
+
+namespace TaxiApplication.DAL.Repositories;
+
+public class EfRepository<T> : IRepository<T> where T : Entity
+{
+	protected readonly TaxiApplicationDbContext _context;
+	protected readonly DbSet<T> _entities;
+	public EfRepository(TaxiApplicationDbContext context)
+	{
+		_context = context;
+		_entities = context.Set<T>();
+	}
+
+	public async Task AddAsync(T entity, CancellationToken cancellationToken = default)
+	{
+		await _entities.AddAsync(entity);
+		await _context.SaveChangesAsync();
+	}
+
+	public async Task DeleteAsync(T entity, CancellationToken cancellationToken = default)
+	{
+		await Task.Run(()=> _entities.Remove(entity));
+		await _context.SaveChangesAsync();
+	}
+
+	public async Task<T?> FirstOrDefaultAsync(Func<T, bool> filter, CancellationToken cancellationToken = default)
+	{
+		return await Task.Run(() => _entities.FirstOrDefault(filter));
+	}
+
+	public async Task<T?> GetByIdAsync(int id)
+	{
+		return await Task.Run(() => _entities.FirstOrDefault(x => x.Id == id));
+	}
+
+	public async Task<IEnumerable<T>> ListAllAsync(CancellationToken cancellationToken = default)
+	{
+		return await _entities.ToListAsync(); 
+	}
+
+	public async Task UpdateAsync(T entity, CancellationToken cancellationToken = default)
+	{
+		await Task.Run(() => _entities.Update(entity));
+		await _context.SaveChangesAsync();
+	}
+}

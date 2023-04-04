142298a012e430d09a2457753cf34b5003f5bbd4
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-04-04 21:25:18 +0300

    Added the IRepository interface to DAL\Interfaces.

---
+++ TaxiApplication/TaxiApplication.DAL/Interfaces/IRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using TaxiApplication.Domain.Entity;
+
+namespace TaxiApplication.DAL.Interfaces;
+
+public interface IRepository<T> where T : Entity
+{
+	Task<T?> GetByIdAsync(int id);
+
+	// IQueryble -> для того, чтобы можно 
+	Task<IEnumerable<T>> ListAllAsync(CancellationToken cancellationToken = default);
+
+	/// <summary>
+	/// Добавление новой сущности
+	/// </summary>
+	/// <param name="entity"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
+	Task AddAsync(T entity, CancellationToken cancellationToken = default);
+
+	/// <summary>
+	/// Изменение сущности
+	/// </summary>
+	/// <param name="entity">Сущность с измененным содержимым</param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
+	Task UpdateAsync(T entity, CancellationToken cancellationToken = default);
+
+	/// <summary>
+	/// Удаление сущности
+	/// </summary>
+	/// <param name="entity">Сущность, которую следует удалить</param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
+	Task DeleteAsync(T entity, CancellationToken cancellationToken = default);
+
+	/// <summary>
+	/// Поиск первой сущности, удовлетворяющей условию отбора.
+	/// Если сущность не найдена, будет возвращено значение по умолчанию
+	/// </summary>
+	/// <param name="filter">Делегат-условие отбора</param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
+	Task<T?> FirstOrDefaultAsync(Func<T, bool> filter, CancellationToken cancellationToken = default);
+}

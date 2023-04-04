d6c54844d3e2db40e720f9b311774393190e5418
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-04-04 21:25:31 +0300

    Added the IUnitOfWork interface to DAL\Interfaces.

---
+++ TaxiApplication/TaxiApplication.DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxiApplication.Domain.Entity;
+
+namespace TaxiApplication.DAL.Interfaces;
+public interface IUnitOfWork
+{
+	IRepository<User> UserRepository { get; }
+	//IRepository<Driver> DriverRepository { get; }
+	public Task RemoveDatbaseAsync();
+	public Task CreateDatabaseAsync();
+	public Task SaveAllAsync();
+}
+

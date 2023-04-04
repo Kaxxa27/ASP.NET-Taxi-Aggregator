62949776db4d5f988cb5568241132665faefd7a7
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-04-04 21:26:51 +0300

    Added the TaxiApplicationDbContext class to DAL.

---
+++ TaxiApplication/TaxiApplication.DAL/TaxiApplicationDbContext.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using TaxiApplication.Domain.Entity;
+using TaxiApplication.Domain.Enum;
+
+namespace TaxiApplication.DAL;
+
+public class TaxiApplicationDbContext : DbContext
+{
+	public DbSet<User> Users { get; set; }
+	public TaxiApplicationDbContext(DbContextOptions<TaxiApplicationDbContext> dbContextOptions) : base(dbContextOptions)
+	{
+		//Database.EnsureDeleted();
+		Database.EnsureCreated();
+	}
+
+
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		
+		User Admin = new User
+		{
+			Id = 1,
+			Name = "Jeka",
+			Surname = "Kaxxa",
+			Email = "Kaxxa2927@mail.ru",
+			Password = "12345",
+			PhoneNumber = "+375297778899",
+			Role = Role.Admin
+		};
+
+
+
+		modelBuilder.Entity<User>().HasData(Admin);
+	
+	}
+
+	//protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+	//{
+	//    optionsBuilder.UseSqlite("Data Source=TaxiApplicationDataBase.db");
+	//}
+}

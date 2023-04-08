2d05b37231309454d242f2e6a523c761a9d22cf2
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-04-08 14:14:03 +0300

    Added the User controller to the area.

--- TaxiApplication/TaxiApplication/Areas/User/Controllers/UserController.cs
+++ TaxiApplication/TaxiApplication/Areas/User/Controllers/UserController.cs
@@ -1,22 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
 using TaxiApplication.BLL.Interfaces;
+using TaxiApplication.Domain.Entity;
+using TaxiApplication.Domain.Responce;
 
 namespace TaxiApplication.WEB.Areas.User.Controllers
 {
-	[Area("User")]
-	public class UserController : Controller
-	{
-		IUserService _userService;
+    [Area("User")]
+    public class UserController : Controller
+    {
+        IUserService _userService;
 
         public UserController(IUserService userService)
         {
             _userService = userService;
         }
 
-		[HttpGet]
-        public IActionResult GetAllUsers()
-		{
-			return View(_userService.GetAllUsers().Result.Data);
-		}
-	}
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(_userService.GetAllUsers().Result.Data);
+        }
+
+        [HttpGet]
+        public IActionResult AddUser() { return View(); }
+
+        [HttpPost]
+        public async Task<IActionResult> AddUser(TaxiApplication.Domain.Entity.User user)
+        {
+            if(ModelState.IsValid)
+            {
+                var response = await _userService.AddUser(user);
+
+                if(response.StatusCode == Domain.Enum.StatusCode.OK) 
+                { 
+                    return RedirectToAction("Index");
+                }
+
+                ModelState.AddModelError("", response.Description);
+			}
+			return View(user);
+        }
+    }
 }

382088aac10c64150326cb3f2fce131c060f7980
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-04-08 14:14:50 +0300

    Intermediate updates database, HomeController and _Layout.

--- TaxiApplication/TaxiApplication/Controllers/HomeController.cs
+++ TaxiApplication/TaxiApplication/Controllers/HomeController.cs
@@ -6,23 +6,28 @@ using TaxiApplication.Domain.Enum;
 
 namespace TaxiApplication.Controllers
 {
-    public class HomeController : Controller
-    {
+	public class HomeController : Controller
+	{
 
-        IUnitOfWork _repository;
-        public HomeController(IUnitOfWork repository)
-        {
-            _repository = repository; 
-        }
-        public IActionResult Index() => View();
+		IUnitOfWork _repository;
+		public HomeController(IUnitOfWork repository)
+		{
+			_repository = repository;
+		}
+		public IActionResult Index() => View();
 
-        [HttpGet]
-        public IActionResult AddItem() => View();
-        [HttpPost]
-        public IActionResult AddItem(User user)
-        {
-            _repository.UserRepository.AddAsync(user);
-            return RedirectToAction("Index");
-        }
-    }
+		[HttpGet]
+		public IActionResult AddItem() => View();
+		[HttpPost]
+		public IActionResult AddItem(User user)
+		{
+			if (ModelState.IsValid)
+			{
+				var response = _repository.UserRepository.AddAsync(user);
+				return RedirectToAction("Index");
+			}
+			//ModelState.AddModelError("", Response.Description);
+			return View(user);
+		}
+	}
 }

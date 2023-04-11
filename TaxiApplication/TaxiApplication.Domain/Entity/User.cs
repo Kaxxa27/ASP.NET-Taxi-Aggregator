8bf8898b2e0d261b9162ab40fc07652066516b40
Eugene_Kakhnouski <Kaxxa2927@mail.ru>
2023-04-11 16:59:31 +0300

    The User class has been completely redesigned.

--- TaxiApplication/TaxiApplication.Domain/Entity/User.cs
+++ TaxiApplication/TaxiApplication.Domain/Entity/User.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Net.Http.Headers;
 using System.Security.Principal;
@@ -12,25 +13,11 @@ namespace TaxiApplication.Domain.Entity;
 
 public class User : Entity
 {
-	[Required(ErrorMessage = "Введите роль пользователя.")]
-    public Role Role { get; set; }
+	public Role Role { get; set; }
+	public string Login { get; set; } = null!;
+	public string Password { get; set; } = null!;
 
-	[Required(ErrorMessage = "Укажите имя пользователя.")]
-	[StringLength(50, MinimumLength = 2, ErrorMessage = "Длина имени должна быть от 2 до 50 символов.")]
-	public string Name { get; set; } = string.Empty;
-	
-	[Required(ErrorMessage = "Укажите фамилию пользователя.")]
-	[StringLength(50, MinimumLength = 2, ErrorMessage = "Длина фамилии должна быть от 2 до 50 символов.")]
-	public string Surname { get; set; } = string.Empty;
-
-	[Required(ErrorMessage = "Укажите пароль.")]
-	public string Password { get; set; } = string.Empty;
-
-	[EmailAddress(ErrorMessage = "Некорректный адрес электронной почты.")]
-	[Required(ErrorMessage = "Укажите вашу электронную почту.")]
-	public string Email { get; set; } = string.Empty;
-
-	[Phone(ErrorMessage = "Некорректный номер телефона.")]
-	[Required(ErrorMessage = "Укажите ваш номер телефона.")]
-	public string PhoneNumber { get; set; } = string.Empty;
+	//[NotMapped]
+	//[Compare("Password")]
+	//public string PasswordConfirm { get; set; } = null!;
 }

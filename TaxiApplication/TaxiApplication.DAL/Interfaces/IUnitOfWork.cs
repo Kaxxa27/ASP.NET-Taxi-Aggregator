a7e7ca247842688a81d8037778826041c2fff3f4
Eugene_Kakhnouski <Kaxxa2927@mail.ru>
2023-04-11 17:03:29 +0300

    IClientRepository and IClientProfileRepository added to the IUnitOfWork.

--- TaxiApplication/TaxiApplication.DAL/Interfaces/IUnitOfWork.cs
+++ TaxiApplication/TaxiApplication.DAL/Interfaces/IUnitOfWork.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TaxiApplication.Domain.Entity;
+using TaxiApplication.Domain.Entity.Profile;
 
 namespace TaxiApplication.DAL.Interfaces;
 public interface IUnitOfWork
 {
 	IRepository<User> UserRepository { get; }
+	IRepository<Client> ClientRepository { get; }
+	IRepository<ClientProfile> ClientProfileRepository { get; }
 	//IRepository<Driver> DriverRepository { get; }
 	public Task RemoveDatbaseAsync();
 	public Task CreateDatabaseAsync();

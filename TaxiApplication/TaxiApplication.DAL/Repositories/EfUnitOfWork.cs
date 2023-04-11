09910d30d3ef7e951ee192a071f6e3848c1b40fe
Eugene_Kakhnouski <Kaxxa2927@mail.ru>
2023-04-11 17:04:39 +0300

    ClientRepository and ClientProfileRepository added to the EfUnitOfWork.

--- TaxiApplication/TaxiApplication.DAL/Repositories/EfUnitOfWork.cs
+++ TaxiApplication/TaxiApplication.DAL/Repositories/EfUnitOfWork.cs
@@ -5,21 +5,28 @@ using System.Text;
 using System.Threading.Tasks;
 using TaxiApplication.DAL.Interfaces;
 using TaxiApplication.Domain.Entity;
+using TaxiApplication.Domain.Entity.Profile;
 
 namespace TaxiApplication.DAL.Repositories;
 
 public class EfUnitOfWork : IUnitOfWork
 {
 	private readonly TaxiApplicationDbContext _context;
+	private readonly IRepository<Client> _clientRepository;
 	private readonly IRepository<User> _userRepository;
+	private readonly IRepository<ClientProfile> _clientProfileRepository;
 
 	public EfUnitOfWork(TaxiApplicationDbContext context)
 	{
 		_context = context;
+		_clientRepository = new EfRepository<Client>(context);
 		_userRepository = new EfRepository<User>(context);
+		_clientProfileRepository = new EfRepository<ClientProfile>(context);
 	}
 
+	IRepository<Client> IUnitOfWork.ClientRepository => _clientRepository;
 	IRepository<User> IUnitOfWork.UserRepository => _userRepository;
+	IRepository<ClientProfile> IUnitOfWork.ClientProfileRepository => _clientProfileRepository;
 
 	public async Task CreateDatabaseAsync()
 	{

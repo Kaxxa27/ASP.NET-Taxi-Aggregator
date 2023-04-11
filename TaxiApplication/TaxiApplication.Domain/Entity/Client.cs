23de3141aed03cb891bde4f677046a7544848ce1
Eugene_Kakhnouski <Kaxxa2927@mail.ru>
2023-04-11 17:01:32 +0300

    Class Client added to the project.

---
+++ TaxiApplication/TaxiApplication.Domain/Entity/Client.cs
@@ -0,0 +1,8 @@
+﻿using TaxiApplication.Domain.Entity.Profile;
+
+namespace TaxiApplication.Domain.Entity;
+
+public class Client : User
+{
+    public virtual ClientProfile Profile { get; set; } = null!;
+}

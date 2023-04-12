2fea6ec6ee39bfc1fcf4afa6781cc0d792f7dccc
Eugene_Kakhnouski <Kaxxa2927@mail.ru>
2023-04-12 11:39:43 +0300

    IAccountService added.

---
+++ TaxiApplication/TaxiApplication.BLL/Interfaces/IAccountService.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Security.Claims;
+using TaxiApplication.BLL.ViewModels;
+using TaxiApplication.Domain.Entity;
+using TaxiApplication.Domain.Responce;
+
+namespace TaxiApplication.BLL.Interfaces;
+
+public interface IAccountService
+{
+    public Task<BaseResponse<ClaimsPrincipal>> LoginAsync(LoginViewModel loginViewModel);
+    public Task<BaseResponse<ClaimsPrincipal>> RegistrationAsync(Client client);
+}

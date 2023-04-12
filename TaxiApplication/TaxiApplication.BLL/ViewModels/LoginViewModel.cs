2a49c3c4cc2c658135d4645a9fb8fcba785bf4e8
Eugene_Kakhnouski <Kaxxa2927@mail.ru>
2023-04-12 11:39:26 +0300

    LoginViewModel added to BLL.

---
+++ TaxiApplication/TaxiApplication.BLL/ViewModels/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiApplication.BLL.ViewModels;
+
+public class LoginViewModel
+{
+    [Required(ErrorMessage = "Укажите логин.")]
+    public string Login { get; set; } = null!;
+
+    [Required(ErrorMessage = "Укажите пароль.")]
+    public string Password { get; set; } = null!;
+}

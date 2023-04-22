8c4817e8397f2fd1eddaf90a56d46e78ef646bda
Eugene_Kakhnouski <Kaxxa2927@mail.ru>
2023-04-22 15:12:16 +0300

    Entity TaxiOrder added.

---
+++ TaxiApplication/TaxiApplication.Domain/Entity/TaxiOrder.cs
@@ -0,0 +1,25 @@
+﻿namespace TaxiApplication.Domain.Entity;
+
+public class TaxiOrder : Entity
+{
+	public Route.Route CurrentRoute { get; set; }
+	public int NumberOfPassengers { get; set; }
+	public string DriverGenderPreference { get; set; } = string.Empty;
+	public string PaymentMethod { get; set; } = string.Empty;
+
+	#region IsProperties
+	public bool IsWheelchairAccessible { get; set; }
+	public bool IsPetFriendly { get; set; }
+	public bool IsRoundTrip { get; set; }
+	public bool IsChildSeatNeeded { get; set; }
+	public bool IsMeetAndGreetNeeded { get; set; }
+	public bool IsFlightTrackingNeeded { get; set; }
+	public bool IsBaggageAssistanceNeeded { get; set; }
+	public bool IsSmokingAllowed { get; set; }
+	public bool IsQuietRideNeeded { get; set; }
+	public bool IsMusicPreferenceNeeded { get; set; }
+	public bool IsAirConditioningNeeded { get; set; }
+	//public string CarTypePreference { get; set; }
+	public bool IsTollRoadsPreferred { get; set; }
+	#endregion
+}

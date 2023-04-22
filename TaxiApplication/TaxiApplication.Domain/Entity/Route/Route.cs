b6a2929141531e318dd2235c53adf74130ce4d9e
Eugene_Kakhnouski <Kaxxa2927@mail.ru>
2023-04-22 15:11:25 +0300

    Entity Route added.

---
+++ TaxiApplication/TaxiApplication.Domain/Entity/Route/Route.cs
@@ -0,0 +1,15 @@
+﻿namespace TaxiApplication.Domain.Entity.Route;
+
+public class Route : Entity
+{
+	//public Location StartLocation { get; set; }
+	//public Location EndLocation { get; set; }  
+	public string? Description { get; set; }
+	[Required(ErrorMessage = "Укажите начальную точку.")]
+	public string StartLocation { get; set; }
+	[Required(ErrorMessage = "Укажите конечную точку.")]
+	public string EndLocation { get; set; }
+    public DateTime Time { get; set; }
+    public double Distance { get; set; }
+	public int TaxiOrderId { get; set; }
+}

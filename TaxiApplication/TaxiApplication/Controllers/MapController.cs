d90514477237310ef26c3a5924574a9c908aaab2
Eugene_Kakhnouski <Kaxxa2927@mail.ru>
2023-04-24 12:21:12 +0300

    Full updated MapController with TaxiOrderViewModel.

--- TaxiApplication/TaxiApplication/Controllers/MapController.cs
+++ TaxiApplication/TaxiApplication/Controllers/MapController.cs
@@ -1,58 +1,96 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System.Reflection.Metadata.Ecma335;
-using TaxiApplication.Domain.Entity;
+﻿using BingMapsRESTToolkit;
+using Microsoft.AspNetCore.Server.IIS.Core;
+using Microsoft.Extensions.Options;
+using System.Security.Claims;
+using System.Text.Json;
+using System.Web.Helpers;
+using TaxiApplication.BLL.ViewModels;
 
 namespace TaxiApplication.WEB.Controllers;
 
 public class MapController : Controller
 {
-    TaxiOrderService _taxiOrderService { get; set; }
+	// BingMapsKey
+	private readonly string BING_MAPS_KEY = "AovVDz7evAPwd8iLhM6pBwRHM3wJD5Z2OJHpYwFeRktRbw5wF5-wYkaf9JycZGHW";
+	TaxiOrderService _taxiOrderService { get; set; }
 
-    public MapController(TaxiOrderService taxiOrderService)
-    {
+	public MapController(TaxiOrderService taxiOrderService)
+	{
 		_taxiOrderService = taxiOrderService;
 	}
-    public IActionResult Index(TaxiOrder taxiOrder)
+
+	public IActionResult Index()
 	{
-        var CurrentOrder = _taxiOrderService.GetOrder(taxiOrder.Id).Result.Data;
-        if (CurrentOrder is null) return View();
-        return View(CurrentOrder);  
+		// Десериализация TaxiViewModel
+		var deserializedViewModel = JsonSerializer.Deserialize<TaxiOrderViewModel>((string)TempData["taxiOrderViewModel_JSON"]!);
+		
+		//TaxiOrderViewModel viewModel = deserializedObject! as TaxiOrderViewModel
+		TaxiOrderViewModel viewModel = deserializedViewModel!;
+	
+		if (viewModel is null) return View();
+		return View(viewModel);
     }
 
     [HttpGet]
-	public IActionResult CreateTaxiOrder() => PartialView();
+	public IActionResult CreateTaxiOrder() => View();
 	[HttpPost]
-	public IActionResult CreateTaxiOrder(TaxiOrder taxiOrder)
+	public async Task<IActionResult> CreateTaxiOrder(TaxiOrderViewModel taxiOrderViewModel)
 	{
-		if (ModelState.IsValid)
+		// Создания запроса к REST API для вычисления маршрута между точками
+		var request = new RouteRequest
 		{
-			var response = _taxiOrderService.AddTaxiOrder(taxiOrder).Result;
-
-			if (response.StatusCode == Domain.Enum.StatusCode.OK)
+			BingMapsKey = BING_MAPS_KEY,
+			RouteOptions = new BingMapsRESTToolkit.RouteOptions()
 			{
-				return RedirectToAction("Index", taxiOrder);
+				TravelMode = TravelModeType.Driving,
+				DistanceUnits = DistanceUnitType.Kilometers
+			},
+			Waypoints = new List<SimpleWaypoint>
+			{	
+				new SimpleWaypoint(taxiOrderViewModel.Route.StartLocation),
+				new SimpleWaypoint(taxiOrderViewModel.Route.EndLocation)
 			}
+		};
 
-			ModelState.AddModelError("", response.Description);
-		}
-		return View();
-		//_taxiOrderService.AddTaxiOrder(taxiOrder);
-		//return RedirectToAction("Index",taxiOrder);    
+		//BingMapsRESTToolkit
+		var response = await ServiceManager.GetResponseAsync(request);
+		var result = response.ResourceSets[0]?.Resources[0] as BingMapsRESTToolkit.Route;
+
+		taxiOrderViewModel.Route.Time = new DateTime().AddSeconds(result!.RouteLegs[0].TravelDuration);
+		taxiOrderViewModel.Route.Distance = result.RouteLegs[0].TravelDistance;
+
+		var json = JsonSerializer.Serialize(taxiOrderViewModel);
+
+		TempData["taxiOrderViewModel_JSON"] = json;
+		return RedirectToAction("Index");
 	}
-	[HttpGet]
-    public IActionResult BuildRoute() => PartialView();
-
-	[HttpPost]
-    public IActionResult BuildRoute(TaxiApplication.Domain.Entity.Route.Route route)
-    {
-        TaxiOrder taxiOrder = new TaxiOrder();
-        taxiOrder.CurrentRoute = route;
-		return RedirectToAction("Index", taxiOrder);  
-    }
 
 	[HttpGet]
 	public IActionResult GetTaxiOrders()
 	{
-		return View(_taxiOrderService.GetAllOrders().Result.Data);
+		return View(_taxiOrderService.GetAllTaxiOrders().Result.Data);
+	}
+
+	[HttpPost]
+	public async Task<IActionResult> SaveTaxiOrder(string taxiOrderViewModel)
+	{
+		try
+		{
+			// Десериализация
+			var deserializedObject = JsonSerializer.Deserialize<TaxiOrderViewModel>(taxiOrderViewModel);
+
+			TaxiOrder taxiOrder = deserializedObject!.Order;
+			taxiOrder.CurrentRoute = deserializedObject.Route;
+			taxiOrder.ClientId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value!);
+
+			await _taxiOrderService.AddTaxiOrder(taxiOrder);
+
+			return RedirectToAction("Index", "Home");
+		}
+		catch (Exception ex)
+		{
+			await Console.Out.WriteLineAsync($"[MapController.SaveTaxiOrder] error: {ex.Message})");
+			return RedirectToAction("CreateTaxiOrder", "Map");
+		}		
 	}
 }

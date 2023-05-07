2a4d3ba024d539a36edd332783a8e6a81b98ba28
Eugene_Kakhnouski <Kaxxa2927@mail.ru>
2023-05-07 15:14:47 +0300

    IMapService and MapService added.

--- TaxiApplication/TaxiApplication.BLL/Interfaces/IMapService.cs
+++ TaxiApplication/TaxiApplication.BLL/Interfaces/IMapService.cs
@@ -1,8 +1,9 @@
-﻿using TaxiApplication.Domain.Entity.Route;
+﻿using BingMapsRESTToolkit;
 
 namespace TaxiApplication.BLL.Interfaces;
 
 public interface IMapService
 {
-	
+	public Task<IBaseResponse<RouteRequest>> CreateRouteRequest(TaxiOrderViewModel taxiOrderViewModel);
+	public Task<IBaseResponse<TaxiOrderViewModel>> CollectInfoFromRequest(RouteRequest request, TaxiOrderViewModel taxiOrderViewModel);
 }
